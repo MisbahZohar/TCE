0f09486dfae6800e228e2dde40df7cdcbf80234e
MisbahZohar <116055177+MisbahZohar@users.noreply.github.com>
2023-10-19 15:28:30 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./Components/header";
+import Body from "./Components/body";
+import Footer from "./Components/footer";
+const App = () => {
+    return (
+        <>
+        <Header/>
+        <Body />
+        <Footer />
+        </>
+    );
+
+};
+export default App;
